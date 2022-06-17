fd19bc77f93d39d5478a8e4ba7b601eca092f28f
valefras <48183554+valefras@users.noreply.github.com>
2022-06-17 20:09:59 +0200

    added apache spark for ingestion

---
+++ ingestion.py
@@ -0,0 +1,110 @@
+from cv2 import transform
+import requests
+from bs4 import BeautifulSoup
+from datetime import datetime
+from time import time, sleep
+import random
+import json
+import numpy as np
+from pyspark.sql.types import IntegerType
+from pyspark.sql import SparkSession
+
+spark = SparkSession \
+    .builder \
+    .appName("BDT2022") \
+    .getOrCreate()
+
+to_scrape = [
+    "https://www.numbeo.com/cost-of-living",
+    "https://www.numbeo.com/crime",
+    "https://www.numbeo.com/quality-of-life",
+    "https://www.numbeo.com/property-investment"
+]
+
+
+def trusty_sleep(n):
+    start = time()
+    while (time() - start < n):
+        sleep(n - (time() - start))
+
+
+def getUrl(main_link, year):
+    return main_link + "/region_rankings.jsp?title=" + str(year) + "&region=150"
+
+
+def scrape():
+    if datetime.now().month <= 2:
+        current_year = datetime.now().year-1
+    else:
+        current_year = datetime.now().year
+
+    cont = 0
+    dataf = None
+    for link in to_scrape:
+        if cont == 0:
+            var_values = ["city", "country", "year", "cost_of_living_index", "rent_index",
+                          "groceries_index", "restaurant_price_index", "local_ppi_index"]
+            var_keys = [2, 3, 5, 6, 7]
+        elif cont == 1:
+            var_values = ["city", "country", "year",
+                          "crime_index", "safety_index"]
+            var_keys = [2, 3]
+        elif cont == 2:
+            var_values = ["city", "country", "year", "qol_index", "ppi_index", "health_care_index",
+                          "traffic_commute_index", "pollution_index", "climate_index"]
+            var_keys = [2, 3, 5, 8, 9, 10]
+        else:
+            var_values = ["city", "country", "year", "gross_rental_yield_centre", "gross_rental_yield_out",
+                          "price_to_rent_centre", "price_to_rent_out", "affordability_index"]
+            var_keys = [3, 4, 5, 6, 8]
+
+        print("Starting " + link)
+        inter_data = []
+        for year in range(2017, current_year + 1):
+            page = requests.get(getUrl(link, year))
+            soup = BeautifulSoup(page.content, "html.parser")
+            results = soup.find(id="t2")
+            rows = results.find_all("tr")
+
+            for entry in rows[1:]:
+                datapoints = entry.find_all('td')
+
+                city_data = [entry.get_text().strip().splitlines()[i-1]
+                             for i in var_keys]
+
+                city = entry.get_text().strip().splitlines()[0].split(', ')
+                city.append(year)
+                city.extend(city_data)
+
+                inter_data.append(city)
+        if cont == 0:
+            dataf = spark.createDataFrame(inter_data, schema=var_values)
+        else:
+            dataf = dataf.join(spark.createDataFrame(inter_data, schema=var_values), [
+                'city', 'country', 'year'], how='outer')
+        print(str(year) + " done")
+        trusty_sleep(random.randint(2, 5))
+        cont += 1
+        print(link + " done")
+    dataf.show(n=5)
+    dataf.schema
+    print('Data extracted.')
+    data_processing(dataf)
+
+
+def data_processing(dataf):
+    print('Starting data processing and generation...')
+
+    dataf.createOrReplaceTempView("city_data")
+    # countries = spark.sql("SELECT DISTINCT country FROM city_data")
+    # coun_list = countries.select('country').rdd.flatMap(lambda x: x).collect()
+    # for country in coun_list:
+
+    for variable in dataf.schema.names[3:]:
+        null_count = spark.sql(
+            f"SELECT COUNT({variable}), country FROM city_data WHERE {variable} IS NULL GROUP BY country")
+        to_process = spark.sql(
+            f"SELECT AVG({variable}), STDEV({variable}), country FROM city_data WHERE {variable} IS NOT NULL GROUP BY country")
+
+
+scrape()
