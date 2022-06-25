5ac0af1e5656c817cdc03974a6a72a9ed5b8da15
Lorenzo Bocchi <lorenzobocchi99@yahoo.com>
2022-06-25 19:31:23 +0200

    added docker files (needs testing), added requirements

--- api/index.js
+++ api/index.js
@@ -134,7 +134,7 @@ fastify.get('/responses/full', async function (request, reply) {
 
 const start = async () => {
     try {
-        await fastify.listen({ port: 3000 })
+        await fastify.listen({ port: 3000, host: '0.0.0.0' })
     } catch (err) {
         fastify.log.error(err)
         process.exit(1)
